087b2af3eab342d2ddb4a6e5ae3e9b381f09a9ba
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-01 14:03:25 +0430

    HTTP Methods defined.

---
+++ Sources/Core/HTTPMethod.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPMethod.swift
+//  MapirServices-iOS
+//
+//  Created by Alireza Asadi on 11/3/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
+
+
+/// HTTP Methods definitions.
+public enum HTTPMethod: String {
+    case delete  = "DELETE"
+    case get     = "GET"
+    case patch   = "PATCH"
+    case post    = "POST"
+    case put     = "PUT"
+}

3e4ec563fdfb0c4252e87bf018e9cd48134d5697
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-01 11:49:21 +0430

    Initial commit

---
+++ Sources/Models/MPIRReverseGeocode.swift
@@ -0,0 +1,9 @@
+//
+//  MPIRReverseGeocode.swift
+//  MapirServices-iOS
+//
+//  Created by Alireza Asadi on 11/3/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation

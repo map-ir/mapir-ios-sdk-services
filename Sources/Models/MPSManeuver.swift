61f240f9ef8ddc903288ba435a50191a11f71323
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-01 15:39:18 +0430

    Maneuver and Intersection models added.

---
+++ Sources/Models/MPSManeuver.swift
@@ -0,0 +1,13 @@
+//
+//  MPSManeuver.swift
+//  MapirServices-iOS
+//
+//  Created by Alireza Asadi on 10/4/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
+
+public struct MPSManeuver {
+
+}

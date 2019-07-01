7172d972509dc7c69bebdf096bb488b55ac1affc
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-01 13:22:18 +0430

    Autocomplete search models added. based on search models.

---
+++ Sources/Models/MPSAutocompleteSearch.swift
@@ -0,0 +1,11 @@
+//
+//  MPSAutocompleteSearch.swift
+//  MapirServices-iOS
+//
+//  Created by Alireza Asadi on 10/4/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
+
+typealias MPSAutocompleteSearch = MPSSearchResult
---
+++ Sources/Models/MPSAutocompleteSearchResult.swift
@@ -0,0 +1,11 @@
+//
+//  MPSAutocompleteSearchResult.swift
+//  MapirServices-iOS
+//
+//  Created by Alireza Asadi on 10/4/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
+
+typealias MPSAutocompleteSearchResult = MPSSearchResult

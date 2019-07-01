64667ef65bcc9d79be53d36b5d4e714a72db65ff
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-01 15:29:58 +0430

    Model for Leg, waypoint and step added.

---
+++ Sources/Models/MPSLeg.swift
@@ -0,0 +1,13 @@
+//
+//  MPSLeg.swift
+//  MapirServices-iOS
+//
+//  Created by Alireza Asadi on 10/4/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
+
+public struct MPSLeg {
+    
+}

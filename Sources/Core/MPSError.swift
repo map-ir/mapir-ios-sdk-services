c3e9cbafd96b125be793361c76562a80d708dc26
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-22 16:10:42 +0430

    Updated errors and their documentations.

--- Sources/Core/MPSError.swift
+++ Sources/Core/MPSError.swift
@@ -10,21 +10,39 @@ import Foundation
 
 public enum MPSError: Error {
 
-    case noAPIAccessToken
-
-    case invalidResponse
-
+    /// Errors related to the service.
     enum ServiceError: Error {
+
+        /// Indicates token-related errors.
+        case invalidAccessToken
+
+        /// Error due to unavailability of services.
         case serviceUnavailabele
     }
 
-    enum RequestError: Error {
+    /// Response related errors.
+    enum ResponseError: Error {
+
+        /// Response form is not valid
+        case invalidResponse
+
+        /// HTTP 400: Bad Reuqest.
         case badRequest
+
+        /// HTTP 404: Not Found.
         case notFound
+    }
+
+    /// Request related errors.
+    enum RequestError: Error {
+
+        /// Error due to invalid input arguments.
         case invalidArgument
     }
 
+    /// Error due to generating request URL
     case urlEncodingError
 
+    /// Error due to decoding image into UIImage
     case imageDecodingError
 }

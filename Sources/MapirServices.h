75e653189be8ab1b2c990200c01a506973c06b1f
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-23 14:49:03 +0430

    property lists updated and created for macOS version of SDK.

---
+++ Sources/MapirServices.h
@@ -0,0 +1,19 @@
+//
+//  MapirServices.h
+//  MapirServices
+//
+//  Created by Alireza Asadi on 31/4/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MapirServices_macOS.
+FOUNDATION_EXPORT double MapirServices_macOSVersionNumber;
+
+//! Project version string for MapirServices_macOS.
+FOUNDATION_EXPORT const unsigned char MapirServices_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapirServices_macOS/PublicHeader.h>
+
+

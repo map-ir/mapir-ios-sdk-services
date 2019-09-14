62164fc0fb748ea55684622ff1af9101ca539ccb
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-09-14 15:11:12 +0430

    Added Network Utility struct to move all network related things out of the MapirServices class body.

---
+++ Sources/Core/NetworkUtilities.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkUtilities.swift
+//  MapirServices-iOS
+//
+//  Created by Alireza Asadi on 23/6/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+struct NetworkingUtilities {
+    
+}

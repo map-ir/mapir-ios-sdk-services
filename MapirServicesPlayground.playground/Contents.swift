c0ce5c3aaaa9e3d3d894c6123c4934ccfaf5c39f
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-09-14 16:58:49 +0430

    Rename problem fixed.

--- MapirServicesPlayground.playground/Contents.swift
+++ MapirServicesPlayground.playground/Contents.swift
@@ -1,1 +1,18 @@
+import MapirServices
+import CoreLocation
 
+//: ## Replace the placeholder with your access token.
+// let accessToken = <#add your access token here#>
+let accessToken = "Helllllo!"
+
+let services = MapirServices(accessToken: accessToken)
+
+let coordinates = CLLocationCoordinate2D(latitude: 34.0123, longitude: 51.12345)
+services.reverseGeocode(for: coordinates) { result in
+    switch result {
+    case .failure(let error):
+        print(error)
+    case .success(let reverseGeocode):
+        print(reverseGeocode.postalAddress)
+    }
+}
---
+++ Sources/Models/AutocompleteSearch.swift
@@ -0,0 +1,11 @@
+//
+//  MPSAutocompleteSearch.swift
+//  MapirServices
+//
+//  Created by Alireza Asadi on 10/4/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
+
+public typealias AutocompleteSearch = Search.Result

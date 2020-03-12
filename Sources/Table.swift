2174ff4c32c85f7ebdd804d7bf94914832d66714
Alireza Asadi <alireza.asadi.36@gmail.com>
2020-03-12 14:55:50 +0330

    Add CustomStringConvertible conformance to Table.

--- Sources/Table.swift
+++ Sources/Table.swift
@@ -25,14 +25,14 @@ public class Table<Key: Hashable, Element> {
         }
     }
 
-    private var grid: [Address: Element] = [:]
+    private var table: [Address: Element] = [:]
 
     init() { }
 
     public subscript(row: Key, column: Key) -> Element? {
         get {
-            let address = gridAddressFor(row: row, column: column)
-            return grid[address]
+            let address = tableAddressFor(row: row, column: column)
+            return table[address]
         }
         set {
             if let newValue = newValue {
@@ -52,17 +52,17 @@ public class Table<Key: Hashable, Element> {
             columns.insert(column)
         }
 
-        let address = gridAddressFor(row: row, column: column)
-        let oldValue = grid[address]
-        grid[address] = value
+        let address = tableAddressFor(row: row, column: column)
+        let oldValue = table[address]
+        table[address] = value
         return oldValue
     }
 
     @discardableResult
     public func removeValueFor(row: Key, column: Key) -> Element? {
-        let address = gridAddressFor(row: row, column: column)
-        let oldValue = grid[address]
-        grid[address] = nil
+        let address = tableAddressFor(row: row, column: column)
+        let oldValue = table[address]
+        table[address] = nil
         return oldValue
     }
 
@@ -82,23 +82,23 @@ public class Table<Key: Hashable, Element> {
 
     public func remove(row: Key) {
         rows.remove(row)
-        for key in gridAddressesWith(row: row) {
-            grid.removeValue(forKey: key)
+        for key in tableAddressesWith(row: row) {
+            table.removeValue(forKey: key)
         }
     }
 
     public func remove(column: Key) {
         columns.remove(column)
-        for key in gridAddressesWith(column: column) {
-            grid.removeValue(forKey: key)
+        for key in tableAddressesWith(column: column) {
+            table.removeValue(forKey: key)
         }
     }
 
     public func valuesOf(column: Key) -> [Key: Element]? {
         guard columns.contains(column) else { return nil }
         var output: [Key: Element] = [:]
-        for address in gridAddressesWith(column: column) {
-            if let value = grid[address] {
+        for address in tableAddressesWith(column: column) {
+            if let value = table[address] {
                 output.updateValue(value, forKey: address.row)
             }
         }
@@ -108,23 +108,23 @@ public class Table<Key: Hashable, Element> {
     public func valuesOf(row: Key) -> [Key: Element]? {
         guard rows.contains(row) else { return nil }
         var output: [Key: Element] = [:]
-        for address in gridAddressesWith(row: row) {
-            if let value = grid[address] {
+        for address in tableAddressesWith(row: row) {
+            if let value = table[address] {
                 output.updateValue(value, forKey: address.column)
             }
         }
         return output
     }
 
-    private func gridAddressesWith(row: Key) -> [Address] {
-        return grid.keys.filter { $0.row == row }
+    private func tableAddressesWith(row: Key) -> [Address] {
+        return table.keys.filter { $0.row == row }
     }
 
-    private func gridAddressesWith(column: Key) -> [Address] {
-        return grid.keys.filter { $0.column == column }
+    private func tableAddressesWith(column: Key) -> [Address] {
+        return table.keys.filter { $0.column == column }
     }
 
-    private func gridAddressFor(row: Key, column: Key) -> Address {
+    private func tableAddressFor(row: Key, column: Key) -> Address {
         return Address(row: row, column: column)
     }
 
@@ -138,5 +138,54 @@ public class Table<Key: Hashable, Element> {
 
     private func isAddressValid(row: Key, column: Key) -> Bool {
         return isRowValid(row: row) && isColumnValid(column: column)
+    }
+}
+
+extension Table: CustomStringConvertible where Key: CustomStringConvertible, Element: CustomStringConvertible {
+    public var description: String {
+
+        guard !rows.isEmpty || !columns.isEmpty else { return "Table is empty." }
+
+        let columns = self.columns
+        let rows = self.rows
+
+        var columnWidths = [rows.map { $0.description.count }.max() ?? 2]
+        for col in columns {
+            var maximum: Int = col.description.count
+            if let values = valuesOf(column: col) {
+                maximum = max(values.map { $1.description.count }.max() ?? 5, maximum)
+            } else {
+                maximum = max(5, maximum)
+            }
+            columnWidths.append(maximum)
+        }
+        columnWidths = columnWidths.map { $0 + 2 }
+
+        var desc = ("|" + String(repeating: "X", count: columnWidths[0] - 2))
+            .padding(toLength: columnWidths[0] - 1, withPad: " ", startingAt: 0)
+
+        for col in Array(columns).enumerated() {
+            let word = "|\(col.element.description)"
+                .padding(toLength: columnWidths[col.offset + 1], withPad: " ", startingAt: 0)
+
+            desc.append(word)
+        }
+        desc.append("|")
+
+        for row in rows.enumerated() {
+            let word = "\n|\(row.element)"
+                .padding(toLength: columnWidths[0], withPad: " ", startingAt: 0)
+
+            desc.append(word)
+
+            for col in columns.enumerated() {
+                let word = ("|" + (self[row.element, col.element]?.description ?? " "))
+                    .padding(toLength: columnWidths[col.offset + 1], withPad: " ", startingAt: 0)
+
+                desc.append(word)
+            }
+            desc.append("|")
+        }
+        return desc
     }
 }

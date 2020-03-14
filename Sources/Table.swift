aa45a39daa9579167598cec18ea6d82fc4fdce47
Alireza Asadi <alireza.asadi.36@gmail.com>
2020-03-14 12:05:35 +0330

    Add Hashable, Equatable, CustomDebugStringConvertible conformance to Table. Add complete documentation to Table's public API.

--- Sources/Table.swift
+++ Sources/Table.swift
@@ -6,29 +6,31 @@
 //  Copyright © 1398 AP Map. All rights reserved.
 //
 
+/// Table is a 2-Dimentional dictionary. It consist of rows and columns. In a table,
+/// every value is stored using two keys, its row and column.
+///
+///
 public class Table<Key: Hashable, Element> {
 
+    /// Contains the rows of the table.
     public private(set) var rows: Set<Key> = []
+
+    /// Contains the columns of the table.
     public private(set) var columns: Set<Key> = []
 
-    private struct Address: Hashable {
-        let row: Key
-        let column: Key
-
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(row)
-            hasher.combine(column)
-        }
-
-        static func == (_ lhs: Address, _ rhs: Address) -> Bool {
-            return lhs.hashValue == rhs.hashValue
-        }
-    }
-
+    /// Stores content of the table.
     private var table: [Address: Element] = [:]
 
-    init() { }
+    /// Creates an empty table.
+    public init() { }
 
+    /// Accesses the value at the specified row and column of the table.
+    ///
+    /// - parameters:
+    ///   - row: The row in the table.
+    ///   - column: The column in the table.
+    ///
+    /// - returns: The value associated with the row and column.
     public subscript(row: Key, column: Key) -> Element? {
         get {
             let address = tableAddressFor(row: row, column: column)
@@ -43,6 +45,17 @@ public class Table<Key: Hashable, Element> {
         }
     }
 
+    /// Updates the value at a certain row and column. Returns the old value at the
+    /// address.
+    ///
+    /// If the row and/or the column are not available in the table, they will be added.
+    ///
+    /// - Parameters:
+    ///   - value: The value which is wanted to be added to the table.
+    ///   - row: The row of the cell.
+    ///   - column: The column of the cell.
+    ///
+    /// - returns: the old value at the address.
     @discardableResult
     public func updateValue(_ value: Element, row: Key, column: Key) -> Element? {
         if !isRowValid(row: row) {
@@ -58,6 +71,13 @@ public class Table<Key: Hashable, Element> {
         return oldValue
     }
 
+    /// Removes the value at the certain address. Returns the removed value.
+    ///
+    /// - Parameters:
+    ///   - row: The row of the cell.
+    ///   - column: The column of the cell.
+    ///
+    /// - returns: The removed value.
     @discardableResult
     public func removeValueFor(row: Key, column: Key) -> Element? {
         let address = tableAddressFor(row: row, column: column)
@@ -66,34 +86,88 @@ public class Table<Key: Hashable, Element> {
         return oldValue
     }
 
-    public func insert(row: Key) {
+    /// Inserts a new row to the table.
+    ///
+    /// If a row with the same `hashValue` exists, nothing happens.
+    ///
+    /// - Parameter row: The key for the new row.
+    ///
+    /// - returns: The added row. If row exists, returns nil.
+    @discardableResult
+    public func insert(row: Key) -> Key? {
         guard !rows.contains(row) else {
-            return
+            return nil
         }
         rows.insert(row)
+        return row
     }
 
-    public func insert(column: Key) {
+    /// Inserts a new column to the table.
+    ///
+    /// If a column with the same `hashValue` exists, nothing happens.
+    ///
+    /// - Parameter column: The key for the new row.
+    ///
+    /// - returns: The added column. If column exists, returns nil.
+    @discardableResult
+    public func insert(column: Key) -> Key? {
         guard !columns.contains(column) else {
-            return
+            return nil
         }
         columns.insert(column)
+        return column
     }
 
-    public func remove(row: Key) {
-        rows.remove(row)
-        for key in tableAddressesWith(row: row) {
-            table.removeValue(forKey: key)
+
+    /// Removes a row and removes every cell associated with it.
+    ///
+    /// - Parameter row: The row to remove.
+    ///
+    /// - returns: A dictionary of removed values associated with their columns. Returns
+    ///   nil if the rows does not exist.
+    @discardableResult
+    public func remove(row: Key) -> [Key: Element]? {
+        if rows.contains(row) {
+            rows.remove(row)
+            var cells: [Key: Element] = [:]
+            for key in tableAddressesWith(row: row) {
+                if let value = table.removeValue(forKey: key) {
+                    cells[key.column] = value
+                }
+            }
+            return cells
+        } else {
+            return nil
         }
     }
 
-    public func remove(column: Key) {
-        columns.remove(column)
-        for key in tableAddressesWith(column: column) {
-            table.removeValue(forKey: key)
+    /// Removes a column and removes every cell associated with it.
+    ///
+    /// - Parameter column: The column to remove.
+    ///
+    /// - returns: A dictionary of removed values associated with their rows. Returns
+    ///   nil if the column does not exist.
+    public func remove(column: Key) -> [Key: Element]? {
+        if columns.contains(column) {
+            columns.remove(column)
+            var cells: [Key: Element] = [:]
+            for key in tableAddressesWith(column: column) {
+                if let value = table.removeValue(forKey: key) {
+                    cells[key.row] = value
+                }
+            }
+            return cells
+        } else {
+            return nil
         }
+
     }
 
+    /// Returns all values in a certain column. Returns nil if column does not exist.
+    ///
+    /// - Parameter column: The target column.
+    ///
+    /// - returns: Values in a certain column or nil if column does not exist.
     public func valuesOf(column: Key) -> [Key: Element]? {
         guard columns.contains(column) else { return nil }
         var output: [Key: Element] = [:]
@@ -105,6 +179,11 @@ public class Table<Key: Hashable, Element> {
         return output
     }
 
+    /// Returns all values in a certain row. Returns nil if row does not exist.
+    ///
+    /// - Parameter row: The target row.
+    ///
+    /// - returns: Values in a certain row or nil if row does not exist.
     public func valuesOf(row: Key) -> [Key: Element]? {
         guard rows.contains(row) else { return nil }
         var output: [Key: Element] = [:]
@@ -114,6 +193,26 @@ public class Table<Key: Hashable, Element> {
             }
         }
         return output
+    }
+}
+
+// MARK: Working with addresses
+
+extension Table {
+
+    /// Combines the row and column in an address.
+    private struct Address: Hashable {
+        let row: Key
+        let column: Key
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(row)
+            hasher.combine(column)
+        }
+
+        static func == (_ lhs: Address, _ rhs: Address) -> Bool {
+            return lhs.hashValue == rhs.hashValue
+        }
     }
 
     private func tableAddressesWith(row: Key) -> [Address] {
@@ -141,7 +240,27 @@ public class Table<Key: Hashable, Element> {
     }
 }
 
-extension Table: CustomStringConvertible where Key: CustomStringConvertible, Element: CustomStringConvertible {
+/// MARK: Protocol conformance.
+
+extension Table: Equatable, Hashable where Element: Hashable {
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(rows)
+        hasher.combine(columns)
+        hasher.combine(table)
+    }
+
+    static public func == (_ lhs: Table, _ rhs: Table) -> Bool {
+        lhs.rows == rhs.rows &&
+            lhs.columns == rhs.columns &&
+            lhs.table == rhs.table
+    }
+
+}
+
+extension Table: CustomStringConvertible, CustomDebugStringConvertible
+where Key: CustomStringConvertible, Element: CustomStringConvertible {
+
     public var description: String {
 
         guard !rows.isEmpty || !columns.isEmpty else { return "Table is empty." }
@@ -188,4 +307,6 @@ extension Table: CustomStringConvertible where Key: CustomStringConvertible, Ele
         }
         return desc
     }
+
+    public var debugDescription: String { return description }
 }
